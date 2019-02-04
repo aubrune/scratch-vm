f7ebe32d3b7d27d5d15a92b2bc6f0fe6559ae7d1
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-02-04 14:48:54 +0100

    Add fr translation.

---
+++ src/extensions/scratch3_rosa/lang/fr.js
@@ -0,0 +1,34 @@
+module.exports = {
+    blocks: {
+        wheelSpeed: 'vitesse de la roue [WHEEL] à [SPEED]',
+        groundSensor: 'capteur sol [GROUND_SENSOR]',
+        distanceSensor: 'distance avant [DIST_SENSOR]',
+        colorSensor: 'couleur avant [COLOR_SENSOR]',
+        lineDetected: 'line detectée ?',
+        blackLineCenter: 'centre de la ligne x',
+        buzz: 'buzz [T]s'
+    },
+    menus: {
+        wheelSide: {
+            left: 'gauche',
+            right: 'droite',
+            all: 'toutes'
+        },
+        distanceSensor: {
+            center: 'centre',
+            left: 'gauche',
+            right: 'droite'
+        },
+        colorSensor: {
+            center: 'centre',
+            left: 'gauche',
+            right: 'droite'
+        },
+        groundSensor: {
+            frontLeft: 'avant gauche',
+            frontRight: 'avant droite',
+            rearLeft: 'arrière gauche',
+            rearRight: 'arrière droite'
+        }
+    }
+};
